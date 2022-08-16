a24c9e5c3bf951ba8a8ea539435274acd1c076f7
Андрей <pjanov84@mail.ru>
2022-08-16 23:34:12 +0300

    added folder_1

--- folder_1/Program.cs
+++ folder_1/Program.cs
@@ -1,4 +1,6 @@
-﻿int num_1 = 35;
-int num_2 = 5;
+﻿int num_1 = new Random().Next(1, 10); // даст случайное число от 1 до 9 [min,max-1]
+Console.WriteLine(num_1);
+int num_2 = new Random().Next(1, 10);
+Console.WriteLine(num_2);
 int res = num_1 + num_2;
 Console.WriteLine(res);
